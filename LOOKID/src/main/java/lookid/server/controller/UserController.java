7607ad2b811f9d55bfd88646fafa60a0d37e9709
= <ulim130@naver.com>
2019-12-09 21:16:39 +0900

    Remove controller's comment

--- LOOKID/src/main/java/lookid/server/controller/UserController.java
+++ LOOKID/src/main/java/lookid/server/controller/UserController.java
@@ -41,20 +41,17 @@ public class UserController {
 	@Qualifier("JWTService")
 	private JWTService JWTService;
 
-	// 아이디 중복확인
 	@RequestMapping(value = "/idcheck", method = RequestMethod.GET)
 	public @ResponseBody SuccessDTO idcheck(@RequestParam(value = "id") String id) throws Exception {
 		return service.checkId(id);
 	}
 
-	// 회원가입
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public @ResponseBody SuccessDTO signup(@RequestBody UserVO user) throws Exception {
-		// 동일인 중복회원가입 방지 -> find_id에서 select시 에러발생 -> phone으로 중복체크
+
 		return service.signup(user);
 	}
 
-	// 아이디 찾기
 	@RequestMapping(value = "/find_id", method = RequestMethod.GET)
 	public @ResponseBody FindIdDTO find_id(@RequestBody FindIdDTO user) throws Exception {
 
@@ -62,68 +59,61 @@ public class UserController {
 
 	}
 
-	// 비밀번호 찾기
 	@RequestMapping(value = "/find_pw", method = RequestMethod.GET)
 	public @ResponseBody SuccessDTO find_pw(@RequestBody FindPwDTO user) throws Exception {
 		return service.find_pw(user);
 	}
 
-	// 관리자 검색
 	@RequestMapping(value = "/find_admin", method = RequestMethod.GET)
 	public @ResponseBody AdminDTO find_admin(@RequestParam(value = "id") String id) throws Exception {
 
 		return service.find_admin(id);
 	}
 
-	// 로그인
 	@RequestMapping(value = "/signin", method = RequestMethod.POST)
 	public @ResponseBody UserDTO signin(@RequestBody SigninDTO user, HttpServletResponse response)
 			throws NullPointerException, Exception {
-		// NullPointException처리
-		// user_pid는 토큰에, 나머지정보는 UserDTO에 담기
+
 		try {
-			// id , pw 존재할 시
+
 			UserVO uvo = jservice.signin(user);
 
-			int user_pid = uvo.getUser_pid(); // 토큰생성에 쓰일 user_pid 따로 저장
-			UserDTO udto = new UserDTO(uvo); // UserVO정보를 UserDTO에 담기
+			int user_pid = uvo.getUser_pid();
+			UserDTO udto = new UserDTO(uvo);
 
-			String token = JWTService.create("user_pid", user_pid); // 토큰 생성
+			String token = JWTService.create("user_pid", user_pid);
 
-			if (JWTService.isUsable(token)) { // 토큰이 유효할 때
-				response.setHeader("Authorization", token); // http 헤더에 토큰 담기. 안드로이드로 전송?
-				// http 헤더 토큰 키 네임 : Authorization
+			if (JWTService.isUsable(token)) {
+				response.setHeader("Authorization", token);
+
 			}
-			return udto; // 안드로이드에게 userDTO정보를 넘겨줌.
+			return udto;
 		} catch (NullPointerException e) {
-			// id, pw가 틀릴 시 NullPointerException 발생
+
 			UserDTO u = new UserDTO(e);
-			return u; // UserDTO의 각 속성을 null값으로 넘겨줌
+			return u;
 		}
 	}
 
-	// 로그아웃 -> interceptor 포함 url 지정
 	@ResponseBody
 	@RequestMapping(value = "/signout", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
 	public JSONObject signout(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// 토큰 자체는 삭제 못하나 destroy한 토큰에 요청이 들어오면 잘못된 접근임을 알수있게 무효화
 
 		JSONObject json = new JSONObject();
 		return json;
 	}
 
-	// 비밀번호 변경 -> interceptor 포함 url 지정
 	@RequestMapping(value = "/modify_pw", method = RequestMethod.PUT)
 	public @ResponseBody SuccessDTO modify_pw(@RequestBody ModifyPwDTO user, HttpServletRequest request)
 			throws Exception {
 
-		final String token = request.getHeader("Authorization"); // HTTP 헤더에 담긴 토큰을 꺼냄 (요청)
+		final String token = request.getHeader("Authorization");
 
 		final SuccessDTO fail = new SuccessDTO(false);
 
 		try {
 			if (token != null && JWTService.isUsable(token)) {
-				int user_pid = JWTService.getUser_pid(token); // 토큰 user_pid 파싱
+				int user_pid = JWTService.getUser_pid(token);
 				return jservice.modify_pw(user, user_pid);
 			} else {
 				return fail;
@@ -133,19 +123,16 @@ public class UserController {
 		}
 	}
 
-	// 내 정보 수정 -> interceptor 포함 url 지정
 	@RequestMapping(value = "/modify", method = RequestMethod.PUT)
 	public @ResponseBody SuccessDTO modify(@RequestBody UserVO user, HttpServletRequest request) throws Exception {
-		// 내정보수정버튼을 클릭했을 때 현재 본인의 정보가 먼저 텍스트필드칸에 채워지게 구현(안드로이드) 빈칸으로 수정 누르면 null값으로 들어감
-		// bank정보를 not null 처리하지 않았기 때문.
 
-		final String token = request.getHeader("Authorization"); // HTTP 헤더에 담긴 토큰을 꺼냄 (요청)
+		final String token = request.getHeader("Authorization");
 
 		final SuccessDTO fail = new SuccessDTO(false);
 
 		try {
 			if (token != null && JWTService.isUsable(token)) {
-				int user_pid = JWTService.getUser_pid(token); // 토큰 user_pid 파싱
+				int user_pid = JWTService.getUser_pid(token);
 				return jservice.modify(user, user_pid);
 			} else {
 				return fail;
--- LOOKID/src/main/java/lookid/server/controller/WebController.java
+++ LOOKID/src/main/java/lookid/server/controller/WebController.java
@@ -4,37 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// 우편번호 검색 API, 실종아동 API, 관리자 페이지  jsp 실행 컨트롤러
 @Controller
 @RequestMapping(value = "/web")
 public class WebController {
 
-	// 주소 검색 open API 다음 kakao
-	// http://localhost:8080/web/address
 	@RequestMapping(value = "/address", method = RequestMethod.GET)
 	public String searchAddress() {
 
 		return "address/addressAction";
 	}
-	
-	// 실종 아동 open API (표 형식)
-	// http://localhost:8080/web/child
+
 	@RequestMapping(value = "/child", method = RequestMethod.GET)
 	public String searchChild() {
 
 		return "child/childAction";
 	}
 
-	// 실종 아동 open API (템플릿)
-	// http://localhost:8080/web/childTemplate
 	@RequestMapping(value = "/childTemplate", method = RequestMethod.GET)
 	public String searchChildTemplate() {
 
 		return "child/childTemplateAction";
 	}
 
-	// 관리자 페이지
-	// http://localhost:8080/web/admin
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
 	public String adminPage() {
 
