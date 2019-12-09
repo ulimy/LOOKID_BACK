0ce964d9df55c5bf4431f8cfd754f0a9d6882876
= <ulim130@naver.com>
2019-12-09 21:18:27 +0900

    Remove dao's comment

--- LOOKID/src/main/java/lookid/server/dao/ReservationCMCDAOImpl.java
+++ LOOKID/src/main/java/lookid/server/dao/ReservationCMCDAOImpl.java
@@ -15,14 +15,12 @@ public class ReservationCMCDAOImpl implements ReservationCMCDAO {
 	@Autowired
 	private Mapper mapper;
 
-	// 예약 정보 추가
 	@Override
 	public int reservation_create(ReservationVO input) throws Exception {
 		mapper.reservation_create(input);
 		return input.getRv_pid();
 	}
 
-	// 그룹 정보 추가
 	@Override
 	public int group_create(int rv_pid, GroupDTO input) throws Exception {
 		GroupVO gv = new GroupVO(rv_pid, input);
@@ -30,35 +28,30 @@ public class ReservationCMCDAOImpl implements ReservationCMCDAO {
 		return gv.getG_pid();
 	}
 
-	// child 정보 추가
 	@Override
 	public void child_create(int g_pid, ChildDTO[] child) throws Exception {
 		mapper.child_create(g_pid, child);
 		return;
 	}
 
-	// 관리자 정보 추가
 	@Override
 	public void admin_create(int g_pid, AdminDTO[] admin) throws Exception {
 		mapper.admin_create(g_pid, admin);
 		return;
 	}
-	
-	// 예약 정보 수정
+
 	@Override
 	public void reservation_modify(ReservationVO input) throws Exception {
 		mapper.reservation_modify(input);
 		return;
 	}
-	
-	// 그룹 정보 삭제
+
 	@Override
 	public void group_delete(int rv_pid) throws Exception {
-		// 그룹 개수가 달라질 경우를 고려하여 삭제 후 재생성
+
 		mapper.group_delete(rv_pid);
 	}
 
-	// 예약 취소
 	@Override
 	public void reservation_cancle(int rv_pid) {
 		mapper.reservation_delete(rv_pid);
