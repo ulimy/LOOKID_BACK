d9dce7dae094497d3324c8203c953686241bd842
= <ulim130@naver.com>
2019-12-09 21:24:00 +0900

    Remove Service's comment

--- LOOKID/src/main/java/lookid/server/service/JWTServiceImpl.java
+++ LOOKID/src/main/java/lookid/server/service/JWTServiceImpl.java
@@ -17,34 +17,27 @@ import lookid.server.exception.UnauthorizedException;
 public class JWTServiceImpl implements JWTService {
 
 	private static final String SALT = "UserSecret";
-	
-	// 토큰 생성
+
 	@Override
-	public <T> String create(String key, T data) { // 토큰만료시간을 설정하면 토큰이 로그인 할 때 마다 바뀜
-		String jwt = Jwts.builder()
-				.setHeaderParam("typ", "JWT")
-				.claim(key, data)
-				.signWith(SignatureAlgorithm.HS256, this.generateKey())
-				.compact(); // 직렬화
+	public <T> String create(String key, T data) {
+		String jwt = Jwts.builder().setHeaderParam("typ", "JWT").claim(key, data)
+				.signWith(SignatureAlgorithm.HS256, this.generateKey()).compact();
 		return jwt;
 	}
 
-	// 키
 	@Override
 	public byte[] generateKey() {
 
 		byte[] key = null;
 		try {
-			key = SALT.getBytes("UTF-8"); // getBytes() 메소드는 인코딩/디코딩 과정에 필요한 바이트 배열을 만드는 메소드
-											// "UTF-8" 바이트 배열로 변환하지 않는다면 테이블에 저장 시 정보가 손실될수 있음
+			key = SALT.getBytes("UTF-8");
+
 		} catch (UnsupportedEncodingException e) {
-			System.out.println("generateKey error");
 		}
 
 		return key;
 	}
 
-	// 토큰 유효성 검사
 	@Override
 	public boolean isUsable(String jwt) {
 		try {
@@ -56,28 +49,17 @@ public class JWTServiceImpl implements JWTService {
 		}
 	}
 
-	// 토큰에서 user_pid 파싱
 	@Override
 	public int getUser_pid(String jwt) throws RuntimeException {
-		
+
 		Jws<Claims> claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
 		int user_pid = (Integer) claims.getBody().get("user_pid");
-			
+
 		return user_pid;
 	}
-	
-	
-	// 토큰 무효화
+
 	@Override
 	public void destroy(String token, HttpServletResponse response) {
-		// 토큰 자체는 삭제 못하나 destroy한 토큰에 요청이 들어오면 잘못된 접근임을 알수있게 무효화
-		// 추가기능으로 Spring Security OAuth JWT invalidate 구현
-		
-//		Jws<Claims> claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(token);
-//		
-//		System.out.println(claims.getBody().getExpiration());
-//		claims.getBody().setExpiration(new Date(System.currentTimeMillis()));		
-//		System.out.println(claims.getBody().getExpiration());
-		
+
 	}
 }
--- LOOKID/src/main/java/lookid/server/service/ReservationListServiceImpl.java
+++ LOOKID/src/main/java/lookid/server/service/ReservationListServiceImpl.java
@@ -27,7 +27,6 @@ public class ReservationListServiceImpl implements ReservationListService {
 		}
 	}
 
-	// 예약 내역 조회
 	@Override
 	public ReservationListDTO[] list(int user_pid) {
 		try {
@@ -37,26 +36,24 @@ public class ReservationListServiceImpl implements ReservationListService {
 		}
 	}
 
-	// 예약 내역 상세 조회
 	@Override
 	public ReservationDetailDTO detail(int rv_pid) {
 		try {
-			// 전체 결과를 담을 ReservationDetailDTO
+
 			ReservationDetailDTO result = new ReservationDetailDTO();
-			// 예약 정보 담기
+
 			result.setReservation(list.reservation_detail(rv_pid));
-			// 그룹 개수 만큼
+
 			for (GroupDTO gd : list.group_detail(rv_pid)) {
-				// 그룹 정보를 담을 GroupInfoDTO에 그룹 정보 담기
+
 				GroupInfoDTO gid = new GroupInfoDTO(gd);
-				//child, admin 정보를 가져오기 위한 g_pid
+
 				int g_pid = gd.getG_pid();
-				// child 정보 담기
+
 				gid.setChild(list.child_detail(g_pid));
-				// admin정보 담기
+
 				gid.setAdmin(list.admin_detail(g_pid));
-				
-				//GroupInfoDTO를 result에 추가
+
 				result.addGroupInfoDTO(gid);
 			}
 			return result;
